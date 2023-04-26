ad77edec1a44b9156ab3197adad40d324253b084
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-04-26 10:45:16 +0100

    Initial Commit

---
+++ SwiftUIBasicComponents/SwiftUIBasicComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicComponentsApp.swift
+//  SwiftUIBasicComponents
+//
+//  Created by zakaria lachkar on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
